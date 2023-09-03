1d694cdccb3079df7f42e7cb93f737c2db8e5a0f
DudeFromKorea <peteryu24@naver.com>
2023-09-04 00:14:25 +0900

    Update Animal_Cafe.java

--- Animal_Cafe/src/cafe/animal/Animal_Cafe.java
+++ Animal_Cafe/src/cafe/animal/Animal_Cafe.java
@@ -10,34 +10,34 @@ public class Animal_Cafe {
 
     ArrayList<Set_Animal> animal_cafe = new ArrayList<>();
 
-    void in(Set_Animal e) {
-        if (e instanceof Dog) {
+    void in(Set_Animal e) { 
+        if (e instanceof Dog) { // 강아지만 입장 가능
             animal_cafe.add(e);
             System.out.print(((Set_Animal) e).getName() + " 강아지가 입장하였습니다. \n현재 입장한 강아지: ");
-            for (int i = 0; i < animal_cafe.size(); i++) {
+            for (int i = 0; i < animal_cafe.size(); i++) { // // 현재 현황 출력
                 System.out.print(animal_cafe.get(i).getName() + " ");
             }
             System.out.println("\n");
-        } else {
+        } else { // 고양이가 입장시
             System.out.println("죄송합니다 강아지만 입장 가능합니다.\n");
         }
     }
 
     void out(Set_Animal e) {
-        boolean isContainsDog = animal_cafe.contains(e);
-        if (isContainsDog) {
+        boolean isContainsDog = animal_cafe.contains(e); 
+        if (isContainsDog) { // 현재 카페에 해당 강아지가 있을 경우
             animal_cafe.remove(e);
             System.out.print(((Set_Animal) e).getName() + " 강아지가 퇴장하였습니다.\n현재 입장한 강아지: ");
-            for (int i = 0; i < animal_cafe.size(); i++) {
+            for (int i = 0; i < animal_cafe.size(); i++) { // 현재 현황 출력
                 System.out.print(animal_cafe.get(i).getName() + " ");
             }
             System.out.println("\n");
-        } else {
+        } else { // // 현재 카페에 해당 강아지가 없을 경우
             System.out.println("현재 " + ((Set_Animal) e).getName() + "는 저희 카페 안에 없습니다.\n");
         }
     }
 
-    void finish() {
+    void finish() { // 영업 종료
         animal_cafe.clear();
         System.out.println("영업을 종료합니다.\n");
     }
