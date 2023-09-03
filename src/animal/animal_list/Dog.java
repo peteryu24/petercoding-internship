f55fddc707e2380ca2530d7b0d2978a1abb4c75f
peteryu24 <peteryu24@naver.com>
2023-09-03 21:20:05 +0900

    asd

--- src/animal/animal_list/Dog.java
+++ src/animal/animal_list/Dog.java
@@ -3,6 +3,7 @@ package animal.animal_list;
 import animal.set_animal.Set_Animal;
 
 public class Dog extends Set_Animal {
+    
 
     public Dog(String species, String name, int age) {
         super(species, name, age);
