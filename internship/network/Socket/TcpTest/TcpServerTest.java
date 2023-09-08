87b347c9ba5b45b0ad5ece7f54b99807cd5848fd
DudeFromKorea <peteryu24@naver.com>
2023-09-08 16:06:02 +0900

    Update TcpServerTest.java

--- internship/network/Socket/TcpTest/TcpServerTest.java
+++ internship/network/Socket/TcpTest/TcpServerTest.java
@@ -15,6 +15,9 @@ public class TcpServerTest { // 서버가 먼저 실행되어 클라이언트의
 		 * 서버 소켓을 사용하여 서버의 특정 포트에서 클라이언트의 연결요청을 처리할 준비 
 		 * 클라리언트의 연결 요청을 받으면 새로운 소켓을 생성하여 클라이언트의 소켓과 연결되도록 한다
 		 * 연결이 성공하면 서버소켓과 상관없이 1:1 통신을 한다
+		 * 
+		 * 단방향 통신인 Http통신과 다르게 소켓은 양방향.(소켓으로 동시에 수행은 불가)
+		 * Http통신은  client가 요청을 보내는 경우에만 server가 응답(역순은 불가)
 		 */
 		ServerSocket ss = null;
 		Socket s;
@@ -33,15 +36,18 @@ public class TcpServerTest { // 서버가 먼저 실행되어 클라이언트의
 				/*
 				 * 서버소켓이 클라이언트의 연결요청을 처리할 수 있도록 대기 상태로 만든다 
 				 */
-				s = ss.accept(); 
+				s = ss.accept();
 				System.out.println("클라이언트 접송 성공!");
 				os = s.getOutputStream(); // 쓰기
-				dos = new DataOutputStream(os);
-				dos.writeUTF("Welcome to connect to TCP Server!(server -> ent)");
+				dos = new DataOutputStream(os); // DataOutputStream으로 데이터 내보내기
+				dos.writeUTF("Welcome to connect to TCP Server!(server -> ent)"); // 문자열 쓰기
 				is = s.getInputStream(); // 소켓에 대한 입력 스트림 반환 (읽기)
-				dis = new DataInputStream(is);
-				String str = new String(dis.readUTF());
+				dis = new DataInputStream(is); // 데이터 가져오기 ("<전송 시작>" + sendString + "<전송 마침>")
+				String str = new String(dis.readUTF()); // 문자열 읽기
 				System.out.println(str); // "<전송 시작>" + sendString + "<전송 마침>"
+
+				// client와의 접속을 끝는 메소드 close()
+
 				is.close(); // 소켓 객체를 닫음
 				dis.close();
 				os.close();
@@ -55,6 +61,13 @@ public class TcpServerTest { // 서버가 먼저 실행되어 클라이언트의
 	}
 
 }
+/*
+ * stream은 일방향의 특징을 가지는 데이터 흐름이다. (데이터가 이동하는 통로)
+ * 외부에서 데이터를 읽거나 외부로 데이터를 출력하는 작업에 사용
+ * 다양한 데이터 소스를 표준화된 방법으로 다루기 위한 것
+ * 하나의 스트림으로 동시에 입출력 불가능
+ * 역방향으로 데이터가 전송될 수 없기 때문에 input, output을 위한 코드가 따로 존재해야함.
+ */
 
 // netstat -ano
 // netstat -ano / find "1234"
