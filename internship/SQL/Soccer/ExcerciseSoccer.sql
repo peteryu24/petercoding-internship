83855cff269216f3665b50e382e5a36895b2f352
DudeFromKorea <peteryu24@naver.com>
2023-09-14 08:22:49 +0900

    Update ExcerciseSoccer.sql

--- internship/SQL/Soccer/ExcerciseSoccer.sql
+++ internship/SQL/Soccer/ExcerciseSoccer.sql
@@ -17,18 +17,19 @@ AND
 
 --2. 각 팀별로 키가 180 이상, 몸무게 90 이하인 선수들의 카운트를 조회한다. 단. 조건으로 사용하는 부분은 반드시 문자가 아닌 숫자로 비교한다.
 SELECT 
-	t.team_name
-	,COUNT(p.player_id) AS count_players
+    t.team_name
+    ,COUNT(p.player_id) AS count_players
 FROM 
-	exam.team AS t
-JOIN 
-	exam.player AS p ON t.team_id = p.team_id
-WHERE CAST
-	(p.height AS INTEGER) >= 180
-  AND CAST
-	(p.weight AS INTEGER) <= 90
+    exam.team t
+    ,exam.player p
+WHERE 
+    t.team_id = p.team_id
+    AND 
+	CAST(NULLIF(p.height, '') AS INTEGER) >= 180
+    AND 
+	CAST(NULLIF(p.weight, '') AS INTEGER) <= 90
 GROUP BY 
-	t.team_name;
+    t.team_name;
 
 --3. 수원월드컵경기장에서 경기가 예정되어 있는 팀에서 'DF' 포지션을 담당하는 선수의 정보를 조회한다.
 SELECT 
