011834b2d37eae1cd8b27c0abc4c617c32a904e6
DudeFromKorea <peteryu24@naver.com>
2023-09-15 08:42:26 +0900

    Update DBConnect.java

--- internship/JDBC/CRUD/DBConnect.java
+++ internship/JDBC/CRUD/DBConnect.java
@@ -2,34 +2,41 @@ package gmx.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
 
 public class DBConnect {
-	// 데이터베이스 연결 정보
+	// DB 연결 정보
 	static final String URL = "jdbc:postgresql://127.0.0.1:5432/Sports";
 	static final String USER = "postgres";
 	static final String PASS = "0000";
 
-	// 데이터베이스 연결을 수행하는 메서드
+	// 연결 수행
 	public static Connection getConnection() {
-		Connection con = null; // Connection 객체를 null로 초기화
+		Connection connect = null; // Connection null로 초기화
 		try {
 			Class.forName("org.postgresql.Driver"); // JDBC 드라이버 로딩
-			con = DriverManager.getConnection(URL, USER, PASS); // 데이터베이스 연결
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace(); // 예외 발생 시 스택 트레이스 출력
+			connect = DriverManager.getConnection(URL, USER, PASS); // DB 연결
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+		} catch (SQLException e) {
+			System.out.println("SQLException");
 		}
-		return con; // Connection 객체 반환
+		return connect;
 	}
-
-	public static void main(String [] args){
+	public static void main(String[] args) {
+		getConnection();
 		Create c = new Create();
-		Insert i = new Insert();
 		Select s = new Select();
+		Insert i = new Insert();
 		Update u = new Update();
 		Delete d = new Delete();
-
+		
 		c.createTable();
+		i.insertValue();
+		s.printAll();
+		u.updateValue();
+		d.delete();
 	}
 
 }
