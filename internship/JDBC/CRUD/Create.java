418c79e09339efa2e007328012a8650cabe88e82
DudeFromKorea <peteryu24@naver.com>
2023-09-15 05:57:08 +0900

    Update Create.java

--- internship/JDBC/CRUD/Create.java
+++ internship/JDBC/CRUD/Create.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Create {
-    public static void main(String[] args) {
-        Connection con = null; // 데이터베이스 연결을 위한 Connection 객체를 null로 초기화
-        Statement stmt = null;  // SQL문을 실행하기 위한 Statement 객체를 null로 초기화
+    public void createTable {
+        Connection con = null; // 데이터베이스 연결을 위한 Connection 객체를 null로 초기화합니다.
+        Statement stmt = null;  // SQL문을 실행하기 위한 Statement 객체를 null로 초기화합니다.
 
-        // 테이블 생성을 위한 SQL 쿼리
+        // 테이블 생성을 위한 SQL 쿼리 문자열을 정의합니다.
         String createTableSQL = "CREATE TABLE exam.team (" + 
                                 "team_id char(3) NOT NULL, " + 
                                 "region_name varchar(8) NOT NULL, " +
@@ -29,30 +29,30 @@ public class Create {
                                 "CONSTRAINT team_pk PRIMARY KEY (team_id));";
 
         try {
-            // DBConnect 클래스의 getConnection 메서드를 호출하여 데이터베이스 연결을 설정
+            // DBConnect 클래스의 getConnection 메서드를 호출하여 데이터베이스 연결을 설정합니다.
             con = DBConnect.getConnection();
             if(con == null){
                 throw new SQLException("DB 연결에 실패하였습니다.");
             }
 
-            // Statement 객체를 생성
+            // Statement 객체를 생성합니다.
             stmt = con.createStatement();
             if(stmt == null){
                 throw new SQLException("실패하였습니다.");
             }
 
-            // SQL 쿼리를 실행하여 테이블을 생성
+            // SQL 쿼리를 실행하여 테이블을 생성합니다.
             stmt.executeUpdate(createTableSQL);
 
-            // 테이블 생성이 성공적으로 완료되면, 콘솔에 메시지를 출력
+            // 테이블 생성이 성공적으로 완료되면, 콘솔에 메시지를 출력합니다.
             System.out.println("Table 'exam.team' has been created.");
 
-        } catch (SQLException e) { // SQLException 예외 발생
-            e.printStackTrace();  
-            System.out.println(createTableSQL); // 실패한 SQL 쿼리를 출력
+        } catch (SQLException e) { // SQLException 예외가 발생하면 실행되는 블록입니다.
+            e.printStackTrace();  // 예외 정보를 출력합니다.
+            System.out.println(createTableSQL); // 실패한 SQL 쿼리를 출력합니다.
 
         } finally {  // 예외 발생 여부와 관계없이 실행되는 블록입니다.
-            // Statement 객체 닫
+            // Statement 객체를 닫습니다.
             try {
                 if (stmt != null)
                     stmt.close();
@@ -60,7 +60,7 @@ public class Create {
                 e.printStackTrace();
             }
 
-            // Connection 객체 닫.
+            // Connection 객체를 닫습니다.
             try {
                 if (con != null)
                     con.close();
