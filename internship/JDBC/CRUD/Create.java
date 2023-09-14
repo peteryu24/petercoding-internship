df437a0c40ddb7b3e1c90aee351de91e3fdf2c12
DudeFromKorea <peteryu24@naver.com>
2023-09-15 05:33:06 +0900

    Update Create.java

--- internship/JDBC/CRUD/Create.java
+++ internship/JDBC/CRUD/Create.java
@@ -6,45 +6,67 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Create {
-	public static void main(String[] args) {
-		Connection con = null; // Connection 객체를 null로 초기화
-		Statement stmt = null; // Statement 객체를 null로 초기화
-		// 테이블 생성 SQL 쿼리 문자열
-		String createTableSQL = "CREATE TABLE exam.team (" + "team_id char(3) NOT NULL, "
-				+ "region_name varchar(8) NOT NULL, " + "team_name varchar(40) NOT NULL, " + "e_team_name varchar(50), "
-				+ "orig_yyyy char(4), " + "stadium_id char(3) NOT NULL, " + "zip_code1 char(3), "
-				+ "zip_code2 char(3), " + "address varchar(80), " + "ddd varchar(3), " + "tel varchar(10), "
-				+ "fax varchar(10), " + "homepage varchar(50), " + "owner_name varchar(10), "
-				+ "CONSTRAINT team_pk PRIMARY KEY (team_id));";
+    public static void main(String[] args) {
+        Connection con = null; // 데이터베이스 연결을 위한 Connection 객체를 null로 초기화
+        Statement stmt = null;  // SQL문을 실행하기 위한 Statement 객체를 null로 초기화
 
-		try {
-			// DBConnection 클래스의 getConnection 메서드를 호출하여 Connection 맺기
-			con = DBConnect.getConnection();
-			// Statement 객체 생성
-			stmt = con.createStatement();
-			// SQL 쿼리 실행
-			stmt.executeUpdate(createTableSQL);
-			// 테이블 생성 성공 메시지 출력
-			System.out.println("Table 'exam.team' has been created.");
-		} catch (SQLException e) {
-			// 예외 발생 시 스택 트레이스와 SQL 쿼리 출력
-			e.printStackTrace();
-			System.out.println(createTableSQL);
-		} finally {
-			// Statement 객체 자원 해제
-			try {
-				if (stmt != null)
-					stmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			// Connection 객체 자원 해제
-			try {
-				if (con != null)
-					con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        // 테이블 생성을 위한 SQL 쿼리
+        String createTableSQL = "CREATE TABLE exam.team (" + 
+                                "team_id char(3) NOT NULL, " + 
+                                "region_name varchar(8) NOT NULL, " +
+                                "team_name varchar(40) NOT NULL, " +
+                                "e_team_name varchar(50), " +
+                                "orig_yyyy char(4), " +
+                                "stadium_id char(3) NOT NULL, " +
+                                "zip_code1 char(3), " +
+                                "zip_code2 char(3), " +
+                                "address varchar(80), " +
+                                "ddd varchar(3), " +
+                                "tel varchar(10), " +
+                                "fax varchar(10), " +
+                                "homepage varchar(50), " +
+                                "owner_name varchar(10), " +
+                                "CONSTRAINT team_pk PRIMARY KEY (team_id));";
+
+        try {
+            // DBConnect 클래스의 getConnection 메서드를 호출하여 데이터베이스 연결을 설정
+            con = DBConnect.getConnection();
+            if(con == null){
+                throw new SQLException("DB 연결에 실패하였습니다.");
+            }
+
+            // Statement 객체를 생성
+            stmt = con.createStatement();
+            if(stmt == null){
+                throw new SQLException("실패하였습니다.");
+            }
+
+            // SQL 쿼리를 실행하여 테이블을 생성
+            stmt.executeUpdate(createTableSQL);
+
+            // 테이블 생성이 성공적으로 완료되면, 콘솔에 메시지를 출력
+            System.out.println("Table 'exam.team' has been created.");
+
+        } catch (SQLException e) { // SQLException 예외 발생
+            e.printStackTrace();  
+            System.out.println(createTableSQL); // 실패한 SQL 쿼리를 출력
+
+        } finally {  // 예외 발생 여부와 관계없이 실행되는 블록입니다.
+            // Statement 객체 닫
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            // Connection 객체 닫.
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
