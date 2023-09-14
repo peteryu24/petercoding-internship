eabe6fdbec3fa49da400d7d59eb9fe367770a3cf
DudeFromKorea <peteryu24@naver.com>
2023-09-14 17:01:10 +0900

    Update Create.java

--- internship/JDBC/CRUD/Create.java
+++ internship/JDBC/CRUD/Create.java
@@ -5,25 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-class DBConnect {
-	// 데이터베이스 연결 정보
-	static final String URL = "jdbc:postgresql://127.0.0.1:5432/Sports";
-	static final String USER = "postgres";
-	static final String PASS = "0000";
-
-	// 데이터베이스 연결을 수행하는 메서드
-	public static Connection getConnection() {
-		Connection con = null; // Connection 객체를 null로 초기화
-		try {
-			Class.forName("org.postgresql.Driver"); // JDBC 드라이버 로딩
-			con = DriverManager.getConnection(URL, USER, PASS); // 데이터베이스 연결
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace(); // 예외 발생 시 스택 트레이스 출력
-		}
-		return con; // Connection 객체 반환
-	}
-}
-
 public class Create {
 	public static void main(String[] args) {
 		Connection con = null; // Connection 객체를 null로 초기화
@@ -38,7 +19,7 @@ public class Create {
 
 		try {
 			// DBConnection 클래스의 getConnection 메서드를 호출하여 Connection 맺기
-			con = DBConnect2.getConnection();
+			con = DBConnect.getConnection();
 			// Statement 객체 생성
 			stmt = con.createStatement();
 			// SQL 쿼리 실행
