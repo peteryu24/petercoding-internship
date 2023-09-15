e1376a5c9475e8a1951163e471c5f061773607c5
DudeFromKorea <peteryu24@naver.com>
2023-09-15 10:33:24 +0900

    Update Create.java

--- internship/JDBC/CRUD/Create.java
+++ internship/JDBC/CRUD/Create.java
@@ -1,14 +1,25 @@
 package gmx.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Create {
-	public static void createTable() {
-		Connection connect = null; // Connection null로 초기화
-		Statement state = null; // SQL문을 실행하기 위한
+	public static void createTable(Connection connect) {
+		//Connection connect = null;
+		/*
+		 * DB에 대한 실제 연결을 나타냄
+		 * Connection을 통해 쿼리를 실행하거나 트랜잭션을 관리하거나 DB 메타데이터에 엑세스
+		 * 
+		 * 메타데이터란 
+		 * DB의 구조, 스키마, 테이블, 칼럼등에 대한 정보
+		 */
+		Statement state = null;
+		/*
+		 * SQL 쿼리를 DB에 전달하고 실행하는 역할
+		 * Connection 객체를 통해 Statement 객체 생성 가능
+		 * 쿼리 CRUD에 사용
+		 */
 		System.out.println("=========================Create Table=========================\n");
 
 		// 쿼리 문자열을 정의
@@ -21,7 +32,7 @@ public class Create {
 
 		try {
 			// DB연결 설정
-			connect = DBConnect.getConnection();
+			//connect = DBConnect.getConnection();
 			if (connect == null) {
 				throw new SQLException("DB 연결에 실패하였습니다.");
 			}
@@ -30,18 +41,16 @@ public class Create {
 			if (state == null) {
 				throw new SQLException("실패하였습니다.");
 			}
-
-			// 테이블을 생성합니다.
+			// 테이블 생성.
 			state.executeUpdate(createTable);
-
-			// 테이블 생성이 성공
+			// 테이블 생성 성공
 			System.out.println("Table 'exam.team' 생성완료.");
 
 		} catch (SQLException e) {
 			System.out.println("SQLException");
 			System.out.println(createTable); // 실패한 SQL 쿼리를 출력
 
-		} finally {
+		} finally { // 역순으로 닫아주기
 			// Statement 닫기
 			try {
 				if (state != null)
