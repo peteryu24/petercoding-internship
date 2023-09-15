bc5c0df67a6b63cd2d13a26e470a782adc0550c3
DudeFromKorea <peteryu24@naver.com>
2023-09-15 10:19:18 +0900

    Update DBConnect.java

--- internship/JDBC/CRUD/DBConnect.java
+++ internship/JDBC/CRUD/DBConnect.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class DBConnect {
 	// DB 연결 정보
-	static final String URL = "jdbc:postgresql://127.0.0.1:5432/Sports";
+	static final String URL = "jdbc:postgresql://127.0.0.1:5432/Sports"; // jdbc를 통해 pgSQL을 사용하여 URL 포트 DB명
 	static final String USER = "postgres";
 	static final String PASS = "0000";
 
@@ -16,7 +16,15 @@ public class DBConnect {
 		Connection connect = null; // Connection null로 초기화
 		try {
 			Class.forName("org.postgresql.Driver"); // JDBC 드라이버 로딩
+			/*
+			 * 컴파일 타임에 직접적인 참조 없이 런타임에 동적으로 클래스를 로드하기 위해
+			 * jar파일의 경로 jvm이 찾을 수 있도록
+			 */
 			connect = DriverManager.getConnection(URL, USER, PASS); // DB 연결
+			/*
+			 * JDBC API의 일부
+			 * DriverManager는 여러 JDBC 드라이버를 관리하고 DB에 연결을 생성하는 역할
+			 */
 		} catch (ClassNotFoundException e) {
 			System.out.println("ClassNotFoundException");
 		} catch (SQLException e) {
@@ -26,17 +34,12 @@ public class DBConnect {
 	}
 	public static void main(String[] args) {
 		getConnection();
-		Create c = new Create();
-		Select s = new Select();
-		Insert i = new Insert();
-		Update u = new Update();
-		Delete d = new Delete();
 		
-		c.createTable();
-		i.insertValue();
-		s.printAll();
-		u.updateValue();
-		d.delete();
+		Create.createTable();
+		Insert.insertValue();
+		Select.printAll();
+		Update.updateValue();
+		Delete.delete();
 	}
 
 }
