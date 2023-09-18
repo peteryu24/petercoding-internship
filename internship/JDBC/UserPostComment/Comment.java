e773ac0f2bbe610a33615d8414f77c3e50a08a9b
DudeFromKorea <peteryu24@naver.com>
2023-09-18 09:31:33 +0900

    Update Comment.java

--- internship/JDBC/UserPostComment/Comment.java
+++ internship/JDBC/UserPostComment/Comment.java
@@ -58,7 +58,7 @@ class CommentVO {
 
 public class Comment {
 	// DB 연결 정보
-	static final String URL = "jdbc:postgresql://127.0.0.1:5432/Sports";
+	static final String URL = "jdbc:postgresql://127.0.0.1:5432/UsersPostsComments";
 	static final String USER = "postgres";
 	static final String PASS = "0000";
 
@@ -72,12 +72,13 @@ public class Comment {
 			System.out.println("ClassNotFoundException");
 		} catch (SQLException e) {
 			System.out.println("SQLException");
+			e.printStackTrace();
 		}
 		return connect;
 	}
 
-	public static void createTable(Connection connect) {
-		// Connection connect = null;
+	public static void createTable() {
+		Connection connect = null;
 		/*
 		 * DB에 대한 실제 연결을 나타냄
 		 * Connection을 통해 쿼리를 실행하거나 트랜잭션을 관리하거나 DB 메타데이터에 엑세스
@@ -89,14 +90,13 @@ public class Comment {
 		System.out.println("=========================Create Table=========================\n");
 
 		// 쿼리 문자열을 정의
-		String createCommentTable = "CREATE TABLE exam.Comment (" + "comment_id INT PRIMARY KEY AUTO_INCREMENT, "
-				+ "user_id INT, " + "post_id INT, " + "comment TEXT, "
-				+ "create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-				+ "FOREIGN KEY (user_id) REFERENCES User(user_id), "
-				+ "FOREIGN KEY (post_id) REFERENCES Post(post_id));";
+		String createCommentTable = "CREATE TABLE exam.comment (" + "comment_id SERIAL PRIMARY KEY, " + "user_id INT, "
+				+ "post_id INT, " + "comment TEXT, " + "create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+				+ "FOREIGN KEY (user_id) REFERENCES exam.users(user_id), "
+				+ "FOREIGN KEY (post_id) REFERENCES exam.post(post_id));";
 
 		try {
-			// connect = DBConnect.getConnection();
+			connect = Comment.getConnection();
 			if (connect == null) {
 				throw new SQLException("DB 연결에 실패하였습니다.");
 			}
@@ -129,7 +129,8 @@ public class Comment {
 		}
 	}
 
-	public static void insertValue(Connection connect) {
+	public static void insertValue() {
+		Connection connect = null;
 		Statement state = null;
 
 		System.out.println("\n=========================Insert Values=========================\n");
@@ -137,57 +138,63 @@ public class Comment {
 		// 데이터 삽입 위한 SQL 쿼리
 		// 데이터 삽입 위한 SQL 쿼리
 		String[] insertQuery = {
-		    "INSERT INTO exam.Comment (user_id, post_id, comment) VALUES (1, 1, 'Google에 대한 댓글')",
-		    "INSERT INTO exam.Comment (user_id, post_id, comment) VALUES (2, 1, 'Apple에 대한 댓글입니다.')",
-		    "INSERT INTO exam.Comment (user_id, post_id, comment) VALUES (1, 2, 'Samsung 댓글임.')"
-		};
-
-
+				"INSERT INTO exam.comment (comment_id,user_id, post_id, comment) VALUES (3,3, 3, 'Samsung 댓글임.')" };
 		try {
-			state = connect.createStatement(); // Statement 객체 생성
+			connect = Comment.getConnection();
+			if (connect == null) {
+				throw new SQLException("DB 연결 실패");
+			}
 
-			// 데이터 삽입
-			for (int i = 0; i < insertQuery.length; i++) {
-				String query = insertQuery[i];
+			try {
+				state = connect.createStatement(); // Statement 객체 생성
+
+				// 데이터 삽입
+				for (int i = 0; i < insertQuery.length; i++) {
+					String query = insertQuery[i];
+					try {
+						state.executeUpdate(query); // SQL 쿼리 실행
+						System.out.println("쿼리문 성공: " + query);
+					} catch (SQLException e) {
+						System.out.println("쿼리문 실패: " + query);
+						e.printStackTrace();
+					}
+				}
+				System.out.println("데이터가 성공적으로 삽입되었습니다."); // 삽입 성공
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// Statement 닫기
 				try {
-					state.executeUpdate(query); // SQL 쿼리 실행
-					System.out.println("쿼리문 성공: " + query);
+					if (state != null) {
+						state.close();
+					}
 				} catch (SQLException e) {
-					System.out.println("쿼리문 실패: " + query);
-
+					System.out.println("SQLException: state is null");
+				}
+				// Connection 닫기
+				try {
+					if (connect != null) {
+						connect.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("SQLException: connect is null");
 				}
 			}
-			System.out.println("데이터가 성공적으로 삽입되었습니다."); // 삽입 성공
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			// Statement 닫기
-			try {
-				if (state != null) {
-					state.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("SQLException: state is null");
-			}
-			// Connection 닫기
-			try {
-				if (connect != null) {
-					connect.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("SQLException: connect is null");
-			}
 		}
 	}
 
-	public static ArrayList<CommentVO> printAll(Connection connect) {
+	public static void printAll() {
+		Connection connect = null;
 		Statement state = null;
 		ResultSet rs = null;
 		ArrayList<CommentVO> commentList = new ArrayList<>();
 
-		String sql = "SELECT comment_id, user_id, post_id, comment, create_time FROM Comment ORDER BY comment_id ASC";
+		String sql = "SELECT comment_id, user_id, post_id, comment, create_time FROM exam.comment ORDER BY comment_id ASC";
 
 		try {
+			connect = Comment.getConnection();
 			state = connect.createStatement();
 			rs = state.executeQuery(sql);
 
@@ -197,7 +204,7 @@ public class Comment {
 				comment.setUser_id(rs.getInt("user_id"));
 				comment.setPost_id(rs.getInt("post_id"));
 				comment.setComment(rs.getString("comment"));
-				comment.setCreate_time(rs.getString("create_time")); // Or use rs.getTimestamp() based on your needs
+				comment.setCreate_time(rs.getString("create_time")); 
 
 				commentList.add(comment);
 			}
@@ -216,15 +223,27 @@ public class Comment {
 				System.out.println("SQLException: " + e.getMessage());
 			}
 		}
+		int commentListLength=commentList.size();
+		 for (int i = 0; i < commentListLength; i++) {
+			CommentVO comment = commentList.get(i);
+		        System.out.println("Comment ID: " + comment.getComment_id());
+		        System.out.println("User ID: " + comment.getUser_id());
+		        System.out.println("Post ID: " + comment.getPost_id());
+		        System.out.println("Comment: " + comment.getComment());
+		        System.out.println("Create Time: " + comment.getCreate_time());
+		        System.out.println();
+		    }
 
-		return commentList;
+		
 	}
 
-	public static void update(Connection connect) {
+	public static void update() {
 		System.out.println("\n=========================PreparedState=========================\n");
+		Connection connect = null;
 		PreparedStatement preState = null;
-		String sql = "UPDATE exam.Comment " + "SET comment = ? WHERE comment_id = ?";
+		String sql = "UPDATE exam.comment " + "SET comment = ? WHERE comment_id = ?";
 		try {
+			connect = Comment.getConnection();
 			// 객체 생성
 			preState = connect.prepareStatement(sql);
 			/*
@@ -233,13 +252,14 @@ public class Comment {
 			 * 리턴된 preState은 쿼리에 데이터를 삽입하고, 실행
 			 */
 			preState.setString(1, "인정합니다"); // 첫 번째 ?의 값
-			preState.setString(2, ""); // 두 번째 ?의 값
+			preState.setInt(2, 3); // 두 번째 ?의 값
 			// 쿼리 update
 			preState.executeUpdate();
 			System.out.println("PreparedState으로 update 완료");
 		} catch (SQLException e) {
 			System.out.println("SQLException");
 			System.out.println(sql);
+			e.printStackTrace();
 		} finally {
 			try {
 				preState.close();
@@ -254,18 +274,20 @@ public class Comment {
 		}
 	}
 
-	public static void delete(Connection connect) {
+	public static void delete() {
+		Connection connect = null;
 		Statement state = null;
 
 		System.out.println("\n=========================Delete=========================\n");
 
 		// 삭제할 계정 id 설정
-		String deleteWhat = "";
+		String deleteWhat = "3";
 
 		// 삭제 SQL 쿼리
-		String sql = "DELETE FROM exam.Comment WHERE comment_id = '" + deleteWhat + "'";
+		String sql = "DELETE FROM exam.comment WHERE comment_id = '" + deleteWhat + "'";
 
 		try {
+			connect = Comment.getConnection();
 			state = connect.createStatement();
 			int rowsAffected = state.executeUpdate(sql); // 변경된 행의 갯수
 
@@ -295,8 +317,13 @@ public class Comment {
 	}
 
 	public static void main(String[] args) {
-		getConnection();
-		// userList 출력 필요
+		createTable();
+		insertValue();
+		printAll();
+		update();
+		printAll();
+		delete();
+		printAll();
 
 	}
 
