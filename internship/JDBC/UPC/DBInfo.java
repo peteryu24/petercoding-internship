6193371877330909164c0f404df00ce287b70f42
DudeFromKorea <peteryu24@naver.com>
2023-09-18 21:32:20 +0900

    Update DBInfo.java

--- internship/JDBC/UPC/DBInfo.java
+++ internship/JDBC/UPC/DBInfo.java
@@ -86,21 +86,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBInfo {
-	static final String URL = "jdbc:postgresql://127.0.0.1:5432/UsersPostsComments";
-	static final String USER = "postgres";
-	static final String PASS = "0000";
+    
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/UsersPostsComments";
+    private static final String USER = "postgres";
+    private static final String PASS = "0000";
+    
+    private static Connection connect = null;
+    private static DBInfo dbconnect = null;
 
-	// 연결 수행
-	public static Connection getConnection() {
-		Connection connect = null;
-		try {
-			Class.forName("org.postgresql.Driver");
-			connect = DriverManager.getConnection(URL, USER, PASS);
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException");
-		} catch (SQLException e) {
-			System.out.println("SQLException");
-		}
-		return connect;
-	}
+    private DBInfo() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connect = DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException");
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+            e.printStackTrace();
+        }
+    }
+    
+    public static synchronized DBInfo getInstance() {
+        if (dbconnect == null) {
+            dbconnect = new DBInfo();
+        }
+        return dbconnect;
+    }
+
+    public Connection getConnection() {
+        return connect;
+    }
 }
+
