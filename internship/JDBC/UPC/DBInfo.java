0a717a5fc40ffa5a1d3d6ca6bdf81c2ac77a754d
DudeFromKorea <peteryu24@naver.com>
2023-09-18 15:25:58 +0900

    Create DBInfo.java

---
+++ internship/JDBC/UPC/DBInfo.java
@@ -0,0 +1,25 @@
+package gmx.upc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBInfo {
+	static final String URL = "jdbc:postgresql://127.0.0.1:5432/UsersPostsComments";
+	static final String USER = "postgres";
+	static final String PASS = "0000";
+
+	// 연결 수행
+	public static Connection getConnection() {
+		Connection connect = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connect = DriverManager.getConnection(URL, USER, PASS);
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException");
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+		}
+		return connect;
+	}
+}
