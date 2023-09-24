4cc5f54046352bbcc08a3c59cba2c4c3b71d5472
DudeFromKorea <peteryu24@naver.com>
2023-09-25 01:57:03 +0900

    Update ChatRoom.java

--- internship/network/Socket/MultiRoomChat/Server/ChatRoom.java
+++ internship/network/Socket/MultiRoomChat/Server/ChatRoom.java
@@ -7,7 +7,7 @@ public class ChatRoom {
 	private String name;
 	private ConcurrentMap<String, Helper> clients = new ConcurrentHashMap<>();
 
-	public ChatRoom(String name) {
+	public ChatRoom(String name) { // 채팅방 이름 설정
 		this.name = name;
 	}
 
@@ -15,17 +15,17 @@ public class ChatRoom {
 		return name;
 	}
 
-	public void addClient(Helper client) {
-		clients.put(client.getName(), client);
+	public void addClient(Helper helper) {
+		clients.put(helper.getName(), helper); // 사용자의 이름과 helper 자체
 	}
 
 	public void removeClient(String name) {
 		clients.remove(name);
 	}
 
-	public void broadcast(String message, String excludeClient) {
+	public void broadcast(String message, String sender) {
 		for (Helper client : clients.values()) {
-			if (!client.getName().equals(excludeClient)) {
+			if (!client.getName().equals(sender)) { // 자신일 경우 전송 X
 				client.sendMessage(message);
 			}
 		}
