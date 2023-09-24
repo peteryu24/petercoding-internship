cdd5f94eee1e146bf48fa51db22323d653192a9e
DudeFromKorea <peteryu24@naver.com>
2023-09-24 23:31:15 +0900

    Update Server.java

--- internship/network/Socket/MultiRoomChat/Server/Server.java
+++ internship/network/Socket/MultiRoomChat/Server/Server.java
@@ -1,6 +1,7 @@
 package gmx.multiroomchat.server;
 
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
@@ -14,24 +15,25 @@ public class Server {
 	private static final int PORT = 7777;
 	private Map<String, ChatRoom> rooms = new ConcurrentHashMap<>();
 
-	
-
 	public void startServer() {
 		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 			System.out.println("Server started on port " + PORT);
+
 			while (true) {
 				Socket clientSocket = serverSocket.accept();
 				Helper client = new Helper(clientSocket, this);
 				new Thread(client).start();
 			}
+		} catch (BindException e) {
+			System.err.println("오류: 포트 " + PORT + "가 이미 사용 중입니다. 다른 포트를 시도해주세요.");
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("오류: 서버 시작 중 문제 발생. 원인: " + e.getMessage());
 		}
 	}
 
 	public ChatRoom createRoom(String roomName) {
 		if (rooms.containsKey(roomName)) {
-			return null; // 이미 해당 이름의 방이 존재하면 null 반환
+			return null;
 		}
 		ChatRoom room = new ChatRoom(roomName);
 		rooms.put(roomName, room);
@@ -42,7 +44,11 @@ public class Server {
 		return rooms.get(name);
 	}
 
-	public Set<String> getAllRoomNames() {
+	public Set<String> getAllRoomNames() { // 생성시기 오름차순으로 변경 희망 ConcurrentHashMap은 순서가 없음.
 		return rooms.keySet();
+	}
+
+	public static void main(String[] args) {
+		new Server().startServer();
 	}
 }
