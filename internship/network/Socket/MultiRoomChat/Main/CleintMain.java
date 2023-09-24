6cf8bd9a21d1b830b45cf461ef7f8c2cb338694b
DudeFromKorea <peteryu24@naver.com>
2023-09-24 22:40:35 +0900

    Create CleintMain.java

---
+++ internship/network/Socket/MultiRoomChat/Main/CleintMain.java
@@ -0,0 +1,9 @@
+package gmx.multiroomchat.main;
+
+import gmx.multiroomchat.client.Client;
+
+public class CleintMain {
+	public static void main(String[] args) {
+		new Client().startClient();
+	}
+}
