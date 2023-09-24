107e24ccac0941764e48273e7307febbea1ae1ea
DudeFromKorea <peteryu24@naver.com>
2023-09-25 03:17:26 +0900

    Update Receiver.java

--- internship/network/Socket/MultiRoomChat/Client/Receiver.java
+++ internship/network/Socket/MultiRoomChat/Client/Receiver.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Receiver implements Runnable {
 	private DataInputStream dis;
-	private boolean runningFlag = true; // 스레드 실행 flag
+	private boolean runFlag = true; // 스레드 실행 flag
 
 	public Receiver(DataInputStream dis) {
 		this.dis = dis;
@@ -14,12 +14,12 @@ public class Receiver implements Runnable {
 	@Override
 	public void run() {
 		try {
-			while (isRunning()) { // 스레드가 실행 중일때
+			while (isFlag()) { // 스레드가 실행 중일때
 				String received = dis.readUTF(); // 메세지 받아오기
 				System.out.println(received);
 			}
 		} catch (IOException e) {
-			if (isRunning()) {
+			if (isFlag()) {
 				System.err.println("서버에서 메시지를 가져오지 못 함");
 			}
 		} finally {
@@ -33,11 +33,11 @@ public class Receiver implements Runnable {
 		}
 	}
 
-	public synchronized boolean isRunning() {
-		return runningFlag;
+	public synchronized boolean isFlag() {
+		return runFlag;
 	}
 
 	public synchronized void stop() {
-		runningFlag = false;
+		runFlag = false;
 	}
 }
