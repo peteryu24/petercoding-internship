7376ea40d7cffdefe05b556e3fb6bb174b1939fd
DudeFromKorea <peteryu24@naver.com>
2023-09-25 02:31:03 +0900

    Update Server.java

--- internship/network/Socket/MultiRoomChat/Server/Server.java
+++ internship/network/Socket/MultiRoomChat/Server/Server.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import gmx.multiroomchat.room.ChatRoom;
@@ -12,8 +11,19 @@ import gmx.multiroomchat.room.Helper;
 
 public class Server {
 	private static final int PORT = 7777; // 채팅 프로그램을 사용할 포트번호
-	private ConcurrentHashMap<String, ChatRoom> roomManager = new ConcurrentHashMap<>();
-	ServerSocket serverSocket;
+	public ConcurrentHashMap<String, ChatRoom> roomManager = new ConcurrentHashMap<>();
+	private ServerSocket serverSocket;
+	private static Server instance = null; // 싱글톤 인스턴스
+
+	private Server() { // 외부에서 생성 불가(싱글톤 패턴)
+	}
+
+	public static Server getInstance() { // 싱글톤 인스턴스 얻기
+		if (instance == null) {
+			instance = new Server();
+		}
+		return instance;
+	}
 
 	public void startServer() {
 		try {
@@ -22,7 +32,7 @@ public class Server {
 
 			while (true) {
 				Socket socket = serverSocket.accept(); // 서버소켓으로 연결을 수립하고 클라이언트 기다림
-				Helper client = new Helper(socket, this);
+				Helper client = new Helper(socket);
 				new Thread(client).start(); // Helper를 쓰레드로 시작
 			}
 		} catch (BindException e) {
@@ -31,22 +41,4 @@ public class Server {
 			System.err.println("서버 시작 에러");
 		}
 	}
-
-	public ChatRoom createRoom(String roomName) { // helper에서 받아온 이름
-		if (roomManager.containsKey(roomName)) { // 이미 존재한다면
-			return null;
-		}
-		ChatRoom room = new ChatRoom(roomName); // 새로운 채팅방 생성
-		roomManager.put(roomName, room); // 해시맵에 방 추가
-		return room;
-	}
-
-	public ChatRoom enterRoom(String roomName) { // Helper클래스에서 방 입장을 위해 방 정보 return
-		return roomManager.get(roomName);
-	}
-
-	public Set<String> getRoomName() { // 생성시기 오름차순으로 변경 희망 ConcurrentHashMap은 순서가 없음.
-		return roomManager.keySet();
-	}
-
 }
