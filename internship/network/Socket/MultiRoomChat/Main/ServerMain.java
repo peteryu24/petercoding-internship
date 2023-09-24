b176a10bae868d1a23475b0f4a8fa17b8e1c5ee5
DudeFromKorea <peteryu24@naver.com>
2023-09-24 22:40:22 +0900

    Create ServerMain.java

---
+++ internship/network/Socket/MultiRoomChat/Main/ServerMain.java
@@ -0,0 +1,10 @@
+package gmx.multiroomchat.main;
+
+import gmx.multiroomchat.server.Server;
+
+public class ServerMain {
+	public static void main(String[] args) {
+		new Server().startServer();
+		
+	}
+}
