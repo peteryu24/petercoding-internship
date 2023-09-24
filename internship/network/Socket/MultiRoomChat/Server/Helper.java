adb5b0a0fc9459d5347c2458d89be38721bf1c4e
DudeFromKorea <peteryu24@naver.com>
2023-09-24 23:31:33 +0900

    Update Helper.java

--- internship/network/Socket/MultiRoomChat/Server/Helper.java
+++ internship/network/Socket/MultiRoomChat/Server/Helper.java
@@ -1,15 +1,15 @@
 package gmx.multichatroom.room;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import gmx.multiroomchat.server.Server;
 
 public class Helper implements Runnable {
     private Socket socket;
     private Server server;
-    private OutputStream out;
+    private DataOutputStream out;
     private String name;
     private ChatRoom currentRoom;
 
@@ -25,8 +25,7 @@ public class Helper implements Runnable {
     public void sendMessage(String message) {
         try {
             if (out != null) {
-                out.write((message + "\n").getBytes());
-                out.flush();
+                out.writeUTF(message);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,23 +34,18 @@ public class Helper implements Runnable {
 
     @Override
     public void run() {
-        try (InputStream in = socket.getInputStream(); 
-             OutputStream out = this.out = socket.getOutputStream()) {
-
-            byte[] buffer = new byte[1024];
-            int bytesRead;
+        try (DataInputStream in = new DataInputStream(socket.getInputStream());
+             DataOutputStream out = this.out = new DataOutputStream(socket.getOutputStream())) {
 
             sendMessage("이름을 입력하세요: ");
-            bytesRead = in.read(buffer);
-            name = new String(buffer, 0, bytesRead).trim();
+            name = in.readUTF();
 
             while (true) {
                 sendMessage("1. 신규 방 생성");
                 sendMessage("2. 기존 방 입장");
                 sendMessage("입력: ");
 
-                bytesRead = in.read(buffer);
-                String userInput = new String(buffer, 0, bytesRead).trim();
+                String userInput = in.readUTF();
 
                 int choice;
                 try {
@@ -59,18 +53,17 @@ public class Helper implements Runnable {
 
                     if (choice < 1 || choice > 2) {
                         sendMessage("올바른 번호를 선택하세요 (1 또는 2).");
-                        continue; // 메뉴 선택으로 되돌아갑니다.
+                        continue;
                     }
                 } catch (NumberFormatException e) {
                     sendMessage("올바르지 않은 입력입니다. 숫자를 입력해 주세요.");
-                    continue; // 메뉴 선택으로 되돌아갑니다.
+                    continue;
                 }
 
                 switch (choice) {
                     case 1:
                         sendMessage("방 이름을 입력하세요: ");
-                        bytesRead = in.read(buffer);
-                        String roomName = new String(buffer, 0, bytesRead).trim();
+                        String roomName = in.readUTF();
 
                         currentRoom = server.createRoom(roomName);
                         if (currentRoom != null) {
@@ -84,8 +77,7 @@ public class Helper implements Runnable {
                         sendMessage("개설된 방 목록: " + server.getAllRoomNames());
                         sendMessage("입장 희망하는 방 이름을 입력하세요: ");
 
-                        bytesRead = in.read(buffer);
-                        String selectedRoomName = new String(buffer, 0, bytesRead).trim();
+                        String selectedRoomName = in.readUTF();
 
                         currentRoom = server.getRoom(selectedRoomName);
                         if (currentRoom != null) {
@@ -98,15 +90,14 @@ public class Helper implements Runnable {
                 }
 
                 while (currentRoom != null) {
-                    bytesRead = in.read(buffer);
-                    String message = new String(buffer, 0, bytesRead).trim();
+                    String roomMessage = in.readUTF();
 
-                    if (message.equalsIgnoreCase("exit")) {
+                    if (roomMessage.equalsIgnoreCase("exit")) {
                         currentRoom.removeClient(name);
-                        sendMessage("방을 나갔습니다.");
                         currentRoom = null;
+                        sendMessage("방에서 나갑니다.");
                     } else {
-                        currentRoom.broadcast(name + ": " + message, name);
+                        currentRoom.broadcast(name + ": " + roomMessage, name);
                     }
                 }
             }
