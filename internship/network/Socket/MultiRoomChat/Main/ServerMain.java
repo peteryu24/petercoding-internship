c4b71fb58aa2aa9fef9f0d59f9bd598300380bde
DudeFromKorea <peteryu24@naver.com>
2023-09-25 02:31:34 +0900

    Update ServerMain.java

--- internship/network/Socket/MultiRoomChat/Main/ServerMain.java
+++ internship/network/Socket/MultiRoomChat/Main/ServerMain.java
@@ -4,7 +4,8 @@ import gmx.multiroomchat.server.Server;
 
 public class ServerMain {
 	public static void main(String[] args) {
-		new Server().startServer();
-		
+		Server server = Server.getInstance(); // 싱글톤 인스턴스를 가져옴
+		server.startServer(); // 서버 시작
+
 	}
 }
