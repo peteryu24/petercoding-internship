009d8a5713527d602f7694a61fcf9a4b2936f569
DudeFromKorea <peteryu24@naver.com>
2023-09-26 08:44:22 +0900

    Create PostVo.java

---
+++ internship/Web/JavaResources/Table/Post/PostVo.java
@@ -0,0 +1,58 @@
+package gmx.upc.post;
+
+public class PostVo {
+	private int postId;
+	private String email;
+	private String title;
+	private String content;
+	private int view;
+	private String createTime;
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getView() {
+		return view;
+	}
+
+	public void setView(int view) {
+		this.view = view;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+}
