57949c96216e5cc80960466aa0c153209b8209d0
DudeFromKorea <peteryu24@naver.com>
2023-09-25 09:29:10 +0900

    Update Receiver.java

--- internship/network/Socket/MultiRoomChat/Client/Receiver.java
+++ internship/network/Socket/MultiRoomChat/Client/Receiver.java
@@ -19,10 +19,8 @@ public class Receiver implements Runnable {
 	public void run() {
 		try {
 			while (isRun) { // 스레드가 실행 중일때
-				synchronized (dis) { // 동기화
-					String received = dis.readUTF(); // 메세지 받아오기
-					System.out.println(received);
-				}
+				String received = dis.readUTF(); // 메세지 받아오기
+				System.out.println(received);
 			}
 		} catch (IOException e) {
 			System.err.println("서버에서 메시지를 가져오지 못 함");
