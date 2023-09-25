f996e991380cc3e108e669f179e742d7a4a53bc6
DudeFromKorea <peteryu24@naver.com>
2023-09-26 08:44:57 +0900

    Create UserVo.java

---
+++ internship/Web/JavaResources/Table/User/UserVo.java
@@ -0,0 +1,40 @@
+package gmx.upc.user;
+
+public class UserVo {
+	private String nickname;
+	private String email;
+	private String password;
+	private String createTime;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+}
