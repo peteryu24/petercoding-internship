b605a65e2afd095f35bac48e98a536b334bdb6b9
DudeFromKorea <peteryu24@naver.com>
2023-09-27 08:29:36 +0900

    Update LoginServlet.java

--- internship/Web/JavaResources/Servlet/LoginServlet.java
+++ internship/Web/JavaResources/Servlet/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.simple.JSONObject;
+
 import gmx.upc.user.UserTable;
 
 @WebServlet("/LoginServlet")
@@ -31,13 +33,17 @@ public class LoginServlet extends HttpServlet {
 	private void checkInfo(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html; charset=UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+
+		JSONObject json = new JSONObject();
 
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 
 		if ((email == null || email.trim().isEmpty()) || (password == null || password.trim().isEmpty())) {
-			response.getWriter().print("모든 정보를 입력하세요.");
+			
+			json.put("message", "모든 정보를 입력하세요.");
+			response.getWriter().print(json.toString());
 			return;
 		}
 
@@ -47,9 +53,10 @@ public class LoginServlet extends HttpServlet {
 		if (isCheck) {
 			HttpSession session = request.getSession();
 			session.setAttribute("userEmail", email);
-			response.getWriter().print("로그인 성공하셨습니다.");
+			json.put("message", "로그인 성공하셨습니다.");
 		} else {
-			response.getWriter().print("로그인 실패하셨습니다.");
+			json.put("message", "로그인 실패하셨습니다.");
 		}
+		response.getWriter().print(json.toString());
 	}
 }
