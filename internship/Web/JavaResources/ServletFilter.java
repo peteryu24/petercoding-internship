95e1e6118f5b1952dc078957168d95c78891b5cd
DudeFromKorea <peteryu24@naver.com>
2023-10-04 00:43:37 +0900

    Create ServletFilter.java

---
+++ internship/Web/JavaResources/ServletFilter.java
@@ -0,0 +1,22 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionChecker {
+
+    public static boolean isUserLoggedIn(HttpServletRequest request) {
+        HttpSession session = request.getSession(false); // 세션이 없으면 새로 생성하지 않음
+
+        if (session != null && session.getAttribute("userEmail") != null) {
+            // 세션에 userEmail 속성이 있으면 사용자가 로그인한 상태로 판단
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+ if (!SessionChecker.isUserLoggedIn(request)) {
+        response.sendRedirect("../Login/Login.jsp?error=unauthorized");
+        return;
+    }
