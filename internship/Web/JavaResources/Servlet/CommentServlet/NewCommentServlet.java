bfe34bd9827b1d2dd31f78a71f6e794841758d53
DudeFromKorea <peteryu24@naver.com>
2023-10-04 00:19:58 +0900

    Update NewCommentServlet.java

--- internship/Web/JavaResources/Servlet/CommentServlet/NewCommentServlet.java
+++ internship/Web/JavaResources/Servlet/CommentServlet/NewCommentServlet.java
@@ -33,10 +33,18 @@ public class NewCommentServlet extends HttpServlet {
 
     private void checkInfo(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // 응답의 MIME 타입 설정  
+        /*  
+		 * 응답의 MIME 타입 설정 
+		 * 클라이언트에게  JSON 형식의 데이터를 전송
+		 * 데이터는 UTF-8 문자 인코딩
+		*/
         response.setContentType("application/json; charset=UTF-8");
 
-        // 파싱 객체 선언
+        /*
+		 * 파싱 객체 선언
+		 * 클라이언트에게 받은 JSON 형식의 문자열을
+		 * java 객체로 변환하는데 사용
+		 */
         JSONParser parser = new JSONParser();
         JSONObject jsonRequest = null;
 
@@ -45,11 +53,10 @@ public class NewCommentServlet extends HttpServlet {
             jsonRequest = (JSONObject) parser.parse(request.getReader());
         } catch (ParseException e) {
             e.printStackTrace();
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);  // 잘못된 요청에 대한 HTTP 상태 코드 설정.
             return;
         }
 
-        // 필요한 정보 추출.
+        
         int postId = ((Long) jsonRequest.get("postId")).intValue(); // JSON에서 long 타입으로 반환되므로 int로 변환
         String comment = (String) jsonRequest.get("comment");
         String email = (String) request.getSession().getAttribute("userEmail");
@@ -65,7 +72,10 @@ public class NewCommentServlet extends HttpServlet {
             jsonResponse.put("success", false);
         }
 
-        // JSON 객체를 문자열로 변환
+        /*
+		 * 문자열로 반환하면 클라이언트의 언어나 프레임워크에 관계없이 다양한 환경에서 활용 가능
+		 * JSON형식의 문자열을 클라이언트에게 반환하기 위해 JSON객체를 문자열로 반환
+		 */
         response.getWriter().write(jsonResponse.toString());
     }
 }
