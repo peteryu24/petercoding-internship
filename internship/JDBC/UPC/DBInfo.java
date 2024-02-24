ba17c94a7fbc9533d5a05ef69ba87be4a34aa0d3
DudeFromKorea <peteryu24@naver.com>
2024-02-24 14:09:43 +0900

    Update DBInfo.java

--- internship/JDBC/UPC/DBInfo.java
+++ internship/JDBC/UPC/DBInfo.java
@@ -4,7 +4,7 @@
     nickname VARCHAR(10) NOT NULL, -- 사용자 닉네임
     email VARCHAR(50) NOT NULL, -- 사용자 메일 주소
     password VARCHAR(15) NOT NULL, -- 사용자 비밀번호
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 계정 생성 날짜
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 계정 생성 날짜 
 );
 COMMENT ON TABLE exam.users IS '유저 정보 테이블';
 COMMENT ON COLUMN exam.users.user_id IS '사용자 식별 id';
